685af301e79d48b66ff7aba35081e94d626af222
ayaBoughzala <ayaboughzala@hotmail.com>
2019-05-02 15:42:09 +0100

    loc

---
+++ xmedicalcomplex/src/app/_models/openinghour.ts
@@ -0,0 +1,17 @@
+export class Openinghour {
+
+    _start1:string ;
+    _end1 :string;
+
+    _start2:string ;
+    _end2 :string;
+
+    _start3:string ;
+    _end3 :string;
+
+    _start4:string ;
+    _end4 :string;
+
+
+
+}
