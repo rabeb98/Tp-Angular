aac37ca67f17daf9a7cd468426415f5478b2fd96
ayaBoughzala <ayaboughzala@hotmail.com>
2019-05-02 12:39:44 +0100

    loc

--- xmedicalcomplex/src/app/_services/index.ts
+++ xmedicalcomplex/src/app/_services/index.ts
@@ -1,3 +1,5 @@
 export * from './alert.service';
 export * from './authentication.service';
 export * from './user.service';
+export * from './date.service';
+export * from './localisation.service';
